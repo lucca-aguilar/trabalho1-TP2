d423d76bb425ab32a09cc850d7b12b6f15dc6489
Lucca Santos Aguilar <sophia.santos.aguilar@gmail.com>
2025-09-24 21:22:20 -0300

    TESTE: Adiciona teste para tabuleiro vazio

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -7,3 +7,11 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch_amalgamated.hpp"
+
+TEST_CASE( "Tabuleiro vazio - jogo indefinido", "[indefinido]" ) { 
+	int tabuleiro_vazio[3][3]= {   { 0, 0, 0 }, 
+	                      { 0, 0, 0 },
+						  { 0, 0, 0 }
+					  };
+    REQUIRE( VerificaVelha(tabuleiro_vazio) == -1 );
+} 
