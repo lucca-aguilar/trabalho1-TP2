4f17a329ef2c197aca41ef8f4fde7252ef9b3429
Lucca Santos Aguilar <sophia.santos.aguilar@gmail.com>
2025-09-24 21:49:28 -0300

    FEATURE: Passa no teste para vitória do 'O' na segunda coluna
    REFATORAÇÃO: Corrige bug no índice das  linhas na verificação de vitória de jogadores na primeira coluna

--- velha.cpp
+++ velha.cpp
@@ -20,8 +20,8 @@ int VerificaVelha(int velha[3][3])
 		return -2;
 	}
 
-	if (velha[1][0] == 2 && velha[2][0] == 2 && velha[2][0] == 2) return 2;
-	if (velha[1][0] == 1 && velha[2][0] == 1 && velha[2][0] == 1) return 1;
+	if ((velha[0][0] == 2 && velha[1][0] == 2 && velha[2][0] == 2) || (velha[0][1] == 2 && velha[1][1] == 2 && velha[2][1] == 2)) return 2;
+	if (velha[0][0] == 1 && velha[1][0] == 1 && velha[2][0] == 1) return 1;
 
 	return -1;
 }
