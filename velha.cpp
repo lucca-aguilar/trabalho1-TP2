e140ea6687c65ab1e948eb4904ff7723d2580f03
Lucca Santos Aguilar <sophia.santos.aguilar@gmail.com>
2025-09-24 21:52:56 -0300

    FEATURE: Passa no teste para vitória do 'X' na terceira coluna

--- velha.cpp
+++ velha.cpp
@@ -21,7 +21,7 @@ int VerificaVelha(int velha[3][3])
 	}
 
 	if ((velha[0][0] == 2 && velha[1][0] == 2 && velha[2][0] == 2) || (velha[0][1] == 2 && velha[1][1] == 2 && velha[2][1] == 2)) return 2;
-	if (velha[0][0] == 1 && velha[1][0] == 1 && velha[2][0] == 1) return 1;
+	if ((velha[0][0] == 1 && velha[1][0] == 1 && velha[2][0] == 1) || (velha[0][2] == 1 && velha[1][2] == 1 && velha[2][2] == 1)) return 1;
 
 	return -1;
 }
